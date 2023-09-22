ce55e2b4e20105f7ddcd2d4f36f7823081e70a2f
Artemis Stefanidou <71610745+ArtemisStefanidou@users.noreply.github.com>
2023-09-22 17:34:24 +0300

    Update API.py

--- CoperDBAPI/API/API.py
+++ CoperDBAPI/API/API.py
@@ -56,8 +56,9 @@ def search_data(start_date, end_date,
             "latitude": {"$gte": min_latitude, "$lte": max_latitude}
         }
         last_data = database.find(query)
-        logging.info(f'database', database.find(query))
-        logging.info(f'last_data', last_data)
+        logging.info(f'database: {database.find(query)}')
+        logging.info(f'last_data: {last_data}')
+      
         if not last_data:
             logging.info(f'2o if where we calculate the lon and lat')
             return {database.name: []}
