76b87b0d003cb498ae6291e0ed1f2909d8684c77
Artemis Stefanidou <71610745+ArtemisStefanidou@users.noreply.github.com>
2023-10-02 15:07:44 +0300

    Update API.py

--- CoperDBAPI/API/API.py
+++ CoperDBAPI/API/API.py
@@ -78,7 +78,7 @@ def search_data(start_date, end_date,
         sample_date = sample_document['time']
         
         # Print the sample date
-        logging.info(f"Sample Date: {type(sample_date)}")
+        logging.info(f"Sample Date: {sample_document[0]}")
 
         last_data = database.find(query)
 
