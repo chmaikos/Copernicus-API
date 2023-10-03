ed3f28ea5369b382b478b0759c853f3f32965acd
Artemis Stefanidou <71610745+ArtemisStefanidou@users.noreply.github.com>
2023-10-03 16:57:37 +0300

    Update producerWave.py

--- CoperDBAPI/ProducerWave/producerWave.py
+++ CoperDBAPI/ProducerWave/producerWave.py
@@ -79,7 +79,7 @@ def create_square(lat1, lon1, distance_km):
     lon2_90 = math.degrees(lon2_90)
     lon2_270 = math.degrees(lon2_270)
 
-    return lat2_180, lon2_90, lat2_0, lon2_270
+    return lat2_180, lon2_270, lat2_0, lon2_90
 
 class MotuOptions:
     def __init__(self, attrs: dict):
