5217b2543f17883bed1cd15de2ef75a92c091c20
Artemis Stefanidou <71610745+ArtemisStefanidou@users.noreply.github.com>
2024-01-18 00:03:15 +0200

    Update app.py

--- VesselAPI/app.py
+++ VesselAPI/app.py
@@ -1,141 +1,86 @@
+import os
 from flask import Flask, Response, request, jsonify
+from flask_cors import CORS
 from pymongo import MongoClient
+from pymongo import ReturnDocument
 from bson.json_util import dumps
+import json
+# from dotenv import load_dotenv
+
+# load_dotenv()
 
 app = Flask(__name__)
-
+CORS(app)
+# mongo_db_url = os.environ.get("MONGO_DB_CONN_STRING")
+# print(mongo_db_url)
 
 client = MongoClient("mongodb://mongodb:27017")
 
+
 db = client["dock-mongo_mongodb_1"]
 
+# =========================== Manage COPS ===========================
 
-def init_steps():
-    steps_per_alert_type = {
-        "alert_id": 1,
-        "description": "Suspicious/Unusual deviation from normalcy",
-        "steps": [
-            {"no": 1, "description": "Call the vessel", "status": "Not initialized"},
-            {
-                "no": 2,
-                "description": "Inform the responsible authority",
-                "status": "Not initialized",
-            },
-            {
-                "no": 3,
-                "description": "Deploy a UAV/EURMARS asset",
-                "status": "Not initialized",
-            },
-            {
-                "no": 4,
-                "description": "Send naval responsible assets",
-                "status": "Not initialized",
-            },
-        ],
-    }
-    collection = db["steps_per_threat"]
-    collection.insert_one(steps_per_alert_type)
-
-
-def init_recommended_steps():
-    steps_per_alert_type = {
-        "alert_id": 1,
-        "description": "Suspicious/Unusual deviation from normalcy",
-        "steps": [
-            {"no": 1, "description": "Call the vessel", "status": "Not initialized"},
-            {
-                "no": 2,
-                "description": "Inform the responsible authority",
-                "status": "Not initialized",
-            },
-        ],
-    }
-    collection = db["rec_steps_per_threat"]
-    collection.insert_one(steps_per_alert_type)
-
-
-@app.route("/api/alert-types/", methods=["GET"])
-def get_alert_types():
-    alert_types = db.alert_types.find({},
-                                      {"alert_type_id": 1, "description": 1})
-    response = Response(
-        response=dumps(alert_types), status=200, mimetype="application/json"
-    )
+@app.route("/api/cops/", methods=['GET'])
+def get_cops():
+    cops = db["COPs"].find({},{"msg ID": 1, "threat name": 1})
+    response = Response(response=dumps(cops), status=200,  mimetype="application/json")
     return response
 
+@app.route("/api/threat-cops/", methods=['GET'])
+def get_cops_for_threat():
+    msg_id = request.args.get('msg_id')
+    filter = { "msg ID": msg_id }
+    cops = db["COPs"].find(filter)
+    response = Response(response=dumps(cops), status=200,  mimetype="application/json")
+    return response
 
-@app.route("/api/op-steps/", methods=["GET"])
-def get_operational_steps():
-    alert_type_id = request.args.get("alert_type_id")
-    if alert_type_id is not None:
-        filter = {"alert_type_id": int(alert_type_id)}
-        steps = list(db.alert_types.find(filter))
-        response = Response(
-            response=dumps(steps), status=200, mimetype="application/json"
-        )
-        return response
-    else:
-        return "[]"
-
-
-@app.route("/api/update-steps/", methods=["PUT"])
+@app.route("/api/update-cops/", methods=['PUT'])
 def update_steps():
-    alert_type_id = request.args.get("alert_type_id")
-    steps = request.get_json()
-    filter = {"alert_type_id": int(alert_type_id)}
-    update = {"$set": {"steps": steps}}
-    db.alert_types.find_one_and_update(filter, update, upsert=False)
+    msg_id = request.args.get('msg_id')
+    json_request = request.get_json()
+    steps = json_request['steps']
+    threat_description = json_request['threat description']
+    print(threat_description)
+    filter = { "msg ID": msg_id }
+    update = {"$set":{"threat description":threat_description,"steps":steps}}
+    db["COPs"].find_one_and_update(filter,update,upsert=False)
     resp = jsonify(success=True)
     return resp
 
+# =========================== DSS ===========================
 
-# =========================== Alert Popup ===========================
-# will need to initialize status when a new alert is generated
+@app.route("/api/rops/", methods=['GET'])
+def get_rops():
+    msg_id = request.args.get('msg_id')
+    filter = { "msg ID": msg_id }
+    rops = db["alerts"].find(filter)
+    json_resp = dumps(rops)
+    data = json.loads(json_resp)
+    steps_set = set()
+    rec_steps_set = set()
+    for i in data[0]["steps"]:
+        desc = i["description"]
+        steps_set.add(desc)
+    for i in data[0]["rec_steps"]:
+        desc = i["description"]
+        rec_steps_set.add(desc)
+    diff = list(steps_set.difference(rec_steps_set))
+    data[0]["other_steps"] = diff
+    response = Response(response=dumps(data), status=200,  mimetype="application/json")
+    return response
 
+@app.route("/api/update-rops/", methods=['PUT'])
+def update_rops():
+    msg_id = request.args.get('msg_id')
+    json_request = request.get_json()
+    steps = json_request['steps']
+    rec_steps = json_request['rec_steps']
+    filter = { "msg ID": msg_id }
+    update = {"$set":{"steps":steps,"rec_steps":rec_steps}}
+    db["alerts"].find_one_and_update(filter,update,upsert=False)
+    resp = jsonify(success=True)
+    return resp
 
-@app.route("/api/alert-steps/", methods=["GET"])
-def get_operational_steps_per_alert():
-    alert_id = request.args.get("alert_id")
-    if alert_id is not None:
-        filter = {"alert_id": int(alert_id)}
-        alert_type_id = db.alerts.find_one(filter, {"alert_type_id": 1})
-        alert_type_id = alert_type_id["alert_type_id"]
-        status = db.statuses.find(
-            {"alert_id": int(alert_id), "alert_type_id": alert_type_id}
-        )
-        response = Response(
-            response=dumps(status), status=200, mimetype="application/json"
-        )
-        return response
-    else:
-        return "[]"
-
-
-@app.route("/api/rec-steps/", methods=["GET"])
-def get_recommended_steps():
-    alert_id = request.args.get("alert_id")
-    if alert_id is not None:
-        filter = {"alert_id": int(alert_id)}
-        steps = list(db.alerts.find(filter))
-        response = Response(
-            response=dumps(steps), status=200, mimetype="application/json"
-        )
-        return response
-    else:
-        return "[]"
-
-
-@app.route("/status", methods=["GET"])
-def get_status():
-    api_is_up = True
-
-    if api_is_up:
-        response = {"status": "success"}
-    else:
-        response = {"status": "fail"}
-
-    return jsonify(response)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
