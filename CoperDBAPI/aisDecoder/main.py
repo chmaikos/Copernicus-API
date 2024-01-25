f3e73d5fcc45fd33c55650df1a2040411abb529f
Artemis Stefanidou <71610745+ArtemisStefanidou@users.noreply.github.com>
2024-01-25 15:41:36 +0200

    Update main.py

--- CoperDBAPI/aisDecoder/main.py
+++ CoperDBAPI/aisDecoder/main.py
@@ -17,7 +17,12 @@ logging.getLogger("").addHandler(console)
 host = "127.0.0.1"
 port = 9094
 
-data = len(list(UDPStream(host, port).read()))
+# Print each item in the list
+try:
+    data = len(list(UDPStream(host, port).read()))
+except Exception as e:
+    logging.info(f'message: {e}')
+
 
 logging.info(f'decoder:')
 logging.info(f'decoder: {data}')
