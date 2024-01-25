bdf881d54728ba268e0a7f7acb57a598e34b515d
Artemis Stefanidou <71610745+ArtemisStefanidou@users.noreply.github.com>
2024-01-25 22:37:43 +0200

    Update main.py

--- CoperDBAPI/aisDecoder/main.py
+++ CoperDBAPI/aisDecoder/main.py
@@ -14,28 +14,11 @@ formatter = logging.Formatter("%(name)s-%(levelname)s-%(message)s")
 console.setFormatter(formatter)
 logging.getLogger("").addHandler(console)
 
-host = "127.0.0.1"
+host = "0.0.0.0"
 port = 9094
-
-logging.info(f'decoder:')
-
-# Initialize data outside the try block
-data = None
-
-# Try to read data
-try:
-    data = len(list(UDPStream(host, port).read()))
-except Exception as e:
-    logging.exception(f'An error occurred: {e}')
-
-logging.info(f'decoder: {data}')
-
-# Check if data is available before logging
-if data is not None:
-    logging.info(f'decoder: {data}')
-    logging.info(f'decoder: {list(UDPStream(host, port).read())}')
 
 # Continue with the rest of your code...
 for msg in UDPStream(host, port):
     message = msg.decode()
     logging.info(f'message: {message}')
+
