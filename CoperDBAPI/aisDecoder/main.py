af12f3babc738bea82617af8e3a946101dbb9146
Artemis Stefanidou <71610745+ArtemisStefanidou@users.noreply.github.com>
2024-01-25 13:59:00 +0200

    Add files via upload

---
+++ CoperDBAPI/aisDecoder/main.py
@@ -0,0 +1,23 @@
+from pyais.stream import UDPStream
+import logging
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    filename="app.log",
+    filemode="w",
+    format="%(name)s-%(levelname)s-%(message)s",
+)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter("%(name)s-%(levelname)s-%(message)s")
+console.setFormatter(formatter)
+logging.getLogger("").addHandler(console)
+
+host = "127.0.0.1"
+port = 9094
+
+
+for msg in UDPStream(host, port):
+    message = msg.decode()
+    logging.info(f'message: {message}')
