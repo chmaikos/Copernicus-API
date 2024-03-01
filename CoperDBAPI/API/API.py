03603b22a885bf2e2e101dfb32ea55f2a74c5e61
Artemis Stefanidou <71610745+ArtemisStefanidou@users.noreply.github.com>
2024-03-01 14:57:24 +0200

    Update API.py

--- CoperDBAPI/API/API.py
+++ CoperDBAPI/API/API.py
@@ -234,7 +234,7 @@ def get_ais_cyprus_dynamic_all():
     try:
         numData = request.args.get("numData")
         numData = int(numData)
-        numData_tmp = numData - 100
+        numData_tmp = numData - 500
         results = mycol_dynamic.find().skip(numData_tmp).limit(numData)
         data_list = list(results)
         json_data = json.loads(json_util.dumps(data_list))
